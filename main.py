6f0138e1a7bad67b462aaee064acf557dea3254b
Shiv Contractor <contractorshiv95@gmail.com>
2021-11-12 09:58:19 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import plotly.graph_objects as go
+import numpy as np
+import pandas as pd
+
+fig = go.Figure(data=[go.Sankey(
+    node = dict(
+      pad = 15,
+      thickness = 20,
+      line = dict(color = "black", width = 0.5),
+      label = ["A1", "A2", "B1", "B2", "C1", "C2"],
+      color = "blue"
+    ),
+    link = dict(
+      source = [0, 1, 0, 2, 3, 3], # indices correspond to labels, eg A1, A2, A1, B1, ...
+      target = [2, 3, 3, 4, 4, 5],
+      value = [8, 4, 2, 8, 4, 2]
+  ))])
+
+fig.update_layout(title_text="Basic Sankey Diagram", font_size=10)
+fig.show()
+
+data = {'Unemployment_Rate': [6.1, 5.8, 5.7, 5.7, 5.8, 5.6, 5.5, 5.3, 5.2, 5.2],
+        'Stock_Index_Price': [1500, 1520, 1525, 1523, 1515, 1540, 1545, 1560, 1555, 1565]
+        }
+
+df = pd.DataFrame(data, columns=['Unemployment_Rate', 'Stock_Index_Price'])
+print(df)
+
